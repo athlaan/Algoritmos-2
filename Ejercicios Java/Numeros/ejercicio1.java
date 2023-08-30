c2bd4a2e3e1c027d7fd818d8521b427533aa6318
アトランゼラ <102254054+athlaan@users.noreply.github.com>
2023-08-30 08:22:28 -0300

    Add files via upload

---
+++ Ejercicios Java/Numeros/ejercicio1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ejercicio1 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int numero;
+		for(int i = 0; i < 100; i +=2) {
+			
+			System.out.println("Ingrese numeros desde 0 hasta el 100\n");
+			numero = scanner.nextInt();
+			
+			if(numero % 2 == 0) {
+				System.out.println("El numero es par : " + numero);
+				
+			}
+		}
+		scanner.close();
+	}
+}
---
+++ Ejercicios Java/Numeros/ejercicio3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ejercicio3 {
+
+	public static void main(String[] args) {
+		
+		//Falta Terminar
+		
+		int 
+	}
+}
